d0873b0f81e93beb4533deeb7879620965bae310
mariebernf <77eyo7ks@students.codeinstitute.net>
2025-04-13 23:45:55 +0200

    Add validation to check for exactly 6 values in provided data

--- run.py
+++ run.py
@@ -10,6 +10,7 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
 ]
 
+
 # Authenticate and authorize
 CREDS = Credentials.from_service_account_file(creds_file_path)
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
@@ -18,8 +19,10 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 # Access the Google Sheet by its name
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
+
 # Function to get sales data
 def get_sales_data():
+
     """
     Get sales figures input from the user.
     """
@@ -28,7 +31,26 @@ def get_sales_data():
     print("Example: 10,20,30,40,50,60\n")
 
     data_str = input("Enter your data here: ")
-    print(f"The data provided is {data_str}")
+
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+
+def validate_data(values):
+    """
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings  cannot be converted into int,
+    or if there aren't exactly 6 values.
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
 
 # Call the function
+
+
 get_sales_data()
